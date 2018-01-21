3bedfcbbec047e0fa380f8fb387251aa64cc919c
akikie <pk_is_me@hotmail.co.th>
2018-01-21 17:01:38 +0700

    ha ha

---
+++ app/src/main/java/com/akikikie/ad/tamapp/secpage.java
@@ -0,0 +1,14 @@
+package com.akikikie.ad.tamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class secpage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secpage);
+    }
+}
